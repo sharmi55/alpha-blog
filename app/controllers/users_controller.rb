c95969767f7032f55d992b2245612140865d0da9
sharmi55 <rai.sharmistha@gmail.com>
2016-03-25 18:04:00 +0000

    complete authentication system and update current_user to be article creator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
+
+before_action :set_user, only: [:edit, :update, :show, :destroy]  
+before_action :require_same_user, only: [:edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -21,12 +24,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
-    
+
       if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -38,7 +39,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -46,6 +46,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
-
   
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def require_same_user
+      if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+      end
+    end
+
 end
+
