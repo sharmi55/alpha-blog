f1ce83a2f55cd7a2b98c96b0f34e12fa428363bb
sharmi55 <rai.sharmistha@gmail.com>
2016-03-25 14:51:37 +0000

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
   
   def create
-    @user= User.new(user_params)
+    @user = User.new(user_params)
     
       if @user.save
         flash[:success] = "Welcome to the Travel blog #{@user.username}"
